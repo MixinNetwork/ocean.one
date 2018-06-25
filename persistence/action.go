9477cf394d2da47de1c8837541f0ac592408717e
Cedric Fung <cedric@vec.io>
2018-06-25 17:51:06 +0900

    order action persistence write

--- persistence/action.go
+++ persistence/action.go
@@ -4,17 +4,118 @@ import (
 	"context"
 	"time"
 
+	"cloud.google.com/go/spanner"
 	"github.com/MixinMessenger/go-number"
+	"github.com/MixinMessenger/ocean.one/engine"
+	"google.golang.org/api/iterator"
 )
+
+type Order struct {
+	OrderId         string    `spanner:"order_id"`
+	OrderType       string    `spanner:"order_type"`
+	QuoteAssetId    string    `spanner:"quote_asset_id"`
+	BaseAssetId     string    `spanner:"base_asset_id"`
+	Side            string    `spanner:"side"`
+	Price           string    `spanner:"price"`
+	FilledPrice     string    `spanner:"filled_price"`
+	RemainingAmount string    `spanner:"remaining_amount"`
+	FilledAmount    string    `spanner:"filled_amount"`
+	CreatedAt       time.Time `spanner:"created_at"`
+	UserId          string    `spanner:"user_id"`
+}
+
+type Action struct {
+	OrderId   string    `spanner:"order_id"`
+	Action    string    `spanner:"action"`
+	CreatedAt time.Time `spanner:"created_at"`
+}
 
 func ReadActionCheckpoint(ctx context.Context) time.Time {
 	return time.Now()
 }
 
 func CreateOrder(ctx context.Context, userId, traceId string, orderType, side, quote, base string, amount, price number.Decimal, createdAt time.Time) error {
-	return nil
+	order := Order{
+		OrderId:         traceId,
+		OrderType:       orderType,
+		Side:            side,
+		QuoteAssetId:    quote,
+		BaseAssetId:     base,
+		Price:           price.Persist(),
+		RemainingAmount: amount.Persist(),
+		FilledAmount:    number.Zero().Persist(),
+		CreatedAt:       createdAt,
+		UserId:          userId,
+	}
+	action := Action{
+		OrderId:   order.OrderId,
+		Action:    engine.OrderActionCreate,
+		CreatedAt: createdAt,
+	}
+	_, err := Spanner(ctx).ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		exist, err := checkAction(ctx, txn, action.OrderId, action.Action)
+		if err != nil || exist {
+			return err
+		}
+		orderMutation, err := spanner.InsertStruct("orders", order)
+		if err != nil {
+			return err
+		}
+		actionMutation, err := spanner.InsertStruct("actions", action)
+		if err != nil {
+			return err
+		}
+		return txn.BufferWrite([]*spanner.Mutation{orderMutation, actionMutation})
+	})
+	return err
 }
 
 func CancelOrder(ctx context.Context, orderId string, createdAt time.Time) error {
-	return nil
+	action := Action{
+		OrderId:   orderId,
+		Action:    engine.OrderActionCancel,
+		CreatedAt: createdAt,
+	}
+	_, err := Spanner(ctx).ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		exist, err := checkAction(ctx, txn, action.OrderId, action.Action)
+		if err != nil || exist {
+			return err
+		}
+		exist, err = checkOrder(ctx, txn, action.OrderId)
+		if err != nil || !exist {
+			return err
+		}
+		actionMutation, err := spanner.InsertStruct("actions", action)
+		if err != nil {
+			return err
+		}
+		return txn.BufferWrite([]*spanner.Mutation{actionMutation})
+	})
+	return err
+}
+
+func checkAction(ctx context.Context, txn *spanner.ReadWriteTransaction, orderId, action string) (bool, error) {
+	it := txn.Read(ctx, "actions", spanner.Key{orderId, action}, []string{"created_at"})
+	defer it.Stop()
+
+	_, err := it.Next()
+	if err == iterator.Done {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func checkOrder(ctx context.Context, txn *spanner.ReadWriteTransaction, orderId string) (bool, error) {
+	it := txn.Read(ctx, "orders", spanner.Key{orderId}, []string{"created_at"})
+	defer it.Stop()
+
+	_, err := it.Next()
+	if err == iterator.Done {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
 }
--- persistence/schema.sql
+++ persistence/schema.sql
@@ -1,17 +1,11 @@
-CREATE TABLE actions (
-  order_id     STRING(36) NOT NULL,
-  action       STRING(36) NOT NULL,
-  created_at   TIMESTAMP NOT NULL,
-) PRIMARY KEY(order_id, action);
-
-CREATE INDEX actions_by_created ON actions(created_at);
-
-
 CREATE TABLE orders (
   order_id          STRING(36) NOT NULL,
   order_type        STRING(36) NOT NULL,
+  quote_asset_id    STRING(36) NOT NULL,
+  base_asset_id     STRING(36) NOT NULL,
   side              STRING(36) NOT NULL,
   price             STRING(128) NOT NULL,
+  filled_price      STRING(128) NOT NULL,
   remaining_amount  STRING(128) NOT NULL,
   filled_amount     STRING(128) NOT NULL,
   created_at        TIMESTAMP NOT NULL,
@@ -19,13 +13,23 @@ CREATE TABLE orders (
 ) PRIMARY KEY(order_id);
 
 
+CREATE TABLE actions (
+  order_id     STRING(36) NOT NULL,
+  action       STRING(36) NOT NULL,
+  created_at   TIMESTAMP NOT NULL,
+) PRIMARY KEY(order_id, action),
+INTERLEAVE IN PARENT orders ON DELETE CASCADE;
+
+CREATE INDEX actions_by_created ON actions(created_at);
+
+
 CREATE TABLE trades (
   trade_id          STRING(36) NOT NULL,
   user_id           STRING(36) NOT NULL,
   ask_order_id      STRING(36) NOT NULL,
   bid_order_id      STRING(36) NOT NULL,
-  ask_asset_id      STRING(36) NOT NULL,
-  bid_asset_id      STRING(36) NOT NULL,
+  quote_asset_id    STRING(36) NOT NULL,
+  base_asset_id     STRING(36) NOT NULL,
   side              STRING(36) NOT NULL,
   liquidity         STRING(36) NOT NULL,
   state             STRING(36) NOT NULL,
