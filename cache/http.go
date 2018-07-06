589982544a3ac8bdca5b249ada710c9c8221249c
Cedric Fung <cedric@vec.io>
2018-07-06 23:25:57 +0900

    handle CORS only for origin header

--- cache/http.go
+++ cache/http.go
@@ -79,7 +79,12 @@ func StartHTTP(ctx context.Context) error {
 
 func handleCORS(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type,Authorization,Mixin-Conversation-ID")
 		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE")
 		w.Header().Set("Access-Control-Max-Age", "600")
