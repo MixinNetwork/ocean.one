34c9095bab52026184256dc10f18d843c5662183
Cedric Fung <cedric@vec.io>
2018-07-29 00:49:44 +0800

    Web:  basic markets list page style

--- example/web/src/market/index.js
+++ example/web/src/market/index.js
@@ -1,11 +1,19 @@
+import './index.scss';
+import $ from 'jquery';
+
 function Home(router, api) {
   this.router = router;
   this.api = api;
+  this.templateIndex = require('./index.html');
 }
 
 Home.prototype = {
   index: function () {
     const self = this;
+    $('body').attr('class', 'market layout');
+    $('#layout-container').html(self.templateIndex({
+      logoURL: require('./logo.png')
+    }));
     self.api.subscribe('c94ac88f-4671-3976-b60a-09064f1811e8-c6d0c728-2624-429b-8e0d-d9d19b6592fa', self.render);
   },
 
