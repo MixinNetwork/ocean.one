bda405922f0cdcc994b9185b88ed1aba06eb4512
Cedric Fung <cedric@vec.io>
2018-07-30 19:46:54 +0800

    Web: basic trade layout

--- example/web/src/market/index.js
+++ example/web/src/market/index.js
@@ -14,8 +14,11 @@ Home.prototype = {
     const self = this;
     $('body').attr('class', 'market layout');
     $('#layout-container').html(self.templateIndex({
-      logoURL: require('./logo.png')
-    })).append(self.templateTrade());
+      logoURL: require('./logo.png'),
+      symbolURL: require('./symbol.png')
+    })).append(self.templateTrade({
+      chartURL: require('./chart.png')
+    }));
     $('.market.detail.spacer').height($('.market.detail.container').outerHeight());
     $(window).scroll(function (event) {
       var scroll = $(window).scrollTop();
@@ -28,21 +31,36 @@ Home.prototype = {
 
       height = $('.layout.header').outerHeight() + $('.markets.container').outerHeight();
       $('.market.detail.spacer').height($('.market.detail.container').outerHeight());
-      if (scroll > height) {
-        $('.markets.nav').hide();
-        $('.market.detail.spacer').hide();
-        $('.market.detail.container').removeClass('fixed');
-      } else if (scroll < height){
-        if ($(window).width() > 1200) {
-          $('.markets.nav').show();
+      if (scroll > height - $(window).height() * 2 / 3) {
+        $('.markets.nav').fadeOut();
+        $('.layout.nav .title').html('BTC-USDT')
+      } else {
+        $('.layout.nav .title').html('USDT MARKETS')
+      }
+      if (scroll < height) {
+        $('.market.detail.container').css({'z-index': -1});
+      } else {
+        $('.market.detail.container').css({'z-index': 1});
+      }
+      if (scroll < height) {
+        if (scroll < height - $(window).height() * 2 / 3 && $(window).width() > 1200) {
+          $('.markets.nav').fadeIn();
         }
         $('.market.detail.spacer').show();
         $('.market.detail.container').addClass('fixed');
+      } else if (scroll > height + 16){
+        $('.market.detail.spacer').hide();
+        $('.market.detail.container').removeClass('fixed');
+      }
+      if (scroll < 256) {
+        $('.market.detail.container').addClass('hidden');
+      } else {
+        $('.market.detail.container').removeClass('hidden');
       }
     });
     $('.layout.nav .logo a').click(function() {
       window.scroll({
-        top: $('.layout.header').outerHeight() - 128,
+        top: $('.layout.header').outerHeight() - $('.layout.nav').outerHeight(),
         behavior: 'smooth'
       });
     });
