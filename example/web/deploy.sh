b18c344e78d23d7e16017feed5bfa5a1c554f93a
Li Yuqing <im.yuqlee@gmail.com>
2019-05-21 12:06:43 +0800

    Update packages

--- example/web/deploy.sh
+++ example/web/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm dist/*
+rm -r dist/*
 npm run dist || exit
 
 gsutil defacl ch -u AllUsers:R gs://ocean-one
@@ -9,7 +9,7 @@ gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m
 gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/*.js gs://ocean-one/assets
 rm dist/*.css dist/*.js
 
-gsutil -h "Cache-Control:public, max-age=31536000" -m cp dist/* gs://ocean-one/assets
+gsutil -h "Cache-Control:public, max-age=31536000" -m cp -r dist/* gs://ocean-one/assets
 
 scp dist/index.html one@ocean-deploy-example:tmp/ocean.one.html
 ssh one@ocean-deploy-example sudo mv /home/one/tmp/ocean.one.html /var/www/html/dist/ocean.one.html
